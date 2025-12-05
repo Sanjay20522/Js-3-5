24ede900bb903a5b040b9fdd02c4514348bdbcf6
Sanjay20522 <sanjayrio321@gmail.com>
2025-12-05 11:39:27 +0530

    Create Events.js

---
+++ Js/Events.js
@@ -0,0 +1,38 @@
+//1.Events in Js:
+//An Event in JavaScript is an action or activity that happens in the browser,and JavaScript can respond to it.
+//Examples of events include clicking a button, submitting a form, hovering over an element, loading a webpage, etc.
+//Events are used to make web pages interactive and dynamic by allowing developers to define specific behaviors that should occur when certain actions take place.
+
+//2.Callback Promises Async and Await:
+//Callback: A callback is a function passed into another function as an argument to be executed later.
+//Promises: A Promise is an object representing the eventual completion or failure of an asynchronous operation.
+//Async/Await: Async/Await is a syntax that allows you to work with Promises in a more synchronous-like manner, making asynchronous code easier to read and write.
+
+//3.Node installation complete
+
+//4.VsCode installation complete
+
+//5. What is React Main Concept  in React in your point of view?
+//React is a JavaScript library for building user interfaces, primarily for single-page applications.
+//It allows developers to create reusable UI components that manage their own state, and efficiently update and render the right components when data changes.
+
+//6.How much do you know about react?
+//Im not familiar with react so far.
+
+//7.What is your goal in this class?
+//My goal is to learn mern stack development and become a proficient full-stack developer.
+
+//8.Where do you see yourself in next 3 months?
+//In the next 3 months, I see myself having a solid understanding of the MERN stack, being able to build and deploy full-stack applications, and actively contributing to projects either individually or as part of a team.
+
+//9.JS recap - main object access and array and functions and html,CSS?
+//Js:Logic & dynamic behavior
+//HTML: Structure & content
+//CSS: Styling & layout
+//Objects:Key-value pairs
+//Arrays:Ordered collections
+//Functions:Reusable code blocks
+//Array Methods:Manipulate arrays (push, pop, map, filter)
+
+//10.File handling-Folder handling must
+//Completed
